fd684e99261a81981932eff1120f9c23b48dce94
StoicInsight <gettantolcen@gmail.com>
2023-01-19 23:53:18 -0500

    moved ball

--- BreakOut/app.js
+++ BreakOut/app.js
@@ -1,19 +1,23 @@
-const canvas = document.getElementById('canvas')
+const canvas = document.getElementById('canvas');
 // Used to paint on the canvas
-const ctx = canvas.getContext('2d')
+const ctx = canvas.getContext('2d');
 
-// ctx.beginPath()
-// // define rectangle: First 2 values specify coordinates ~ Second two values are for width & height
-// ctx.rect(60, 80, 100, 50) 
-// ctx.fillStyle = '#FF0000'
-// // Paint Square
-// ctx.fill()
-// ctx.closePath()
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+let dx = 2;
+let dy = -2;
 
+const draw = () => {
+  ctx.clearRect(0,0, canvas.width, canvas.height)
+  ctx.beginPath();
+  ctx.arc( x, y, 10, 0, Math.PI * 2 );
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+  x += dx; 
+  y += dy;
+  console.log(x, y)
+}
 
-ctx.beginPath()
-ctx.arc(240, 160, 30, 0, Math.PI * 2, false )
-ctx.fillStyle = 'rgba( 0, 0, 255, 0.5)'
-ctx.stroke()
-ctx.closePath()
-
+// Runs a loop every 10 milliseconds
+setInterval(draw, 10);
